337f7593f46e898cc3e061f20128e341bf835bcc
gabriel72188 <gabri72188340@gmail.com>
2025-04-19 15:49:16 +0200

    GMQ12: creación de un formulario en la sección de detalles de cada casa

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -11,6 +11,13 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
+
   constructor() { }
 
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
